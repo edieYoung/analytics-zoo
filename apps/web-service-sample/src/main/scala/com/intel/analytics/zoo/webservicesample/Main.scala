8a28e2e2929fbee68dec9f7e4578e872986057f7
Dongjie Shi <glorysdj@gmail.com>
2018-05-23 13:21:35 +0800

    add engine.init

--- apps/web-service-sample/src/main/scala/com/intel/analytics/zoo/webservicesample/Main.scala
+++ apps/web-service-sample/src/main/scala/com/intel/analytics/zoo/webservicesample/Main.scala
@@ -5,6 +5,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
+import com.intel.analytics.bigdl.utils.Engine
 
 //to package:     sbt assembly
 //to run in sbt:  sbt run -Dmodel.home="~/glorysdj/analytics-zoo-models"
@@ -21,17 +22,22 @@ object Main extends App with HttpServerAble {
   override implicit val system = ActorSystem(actorSystemName)
   override implicit val executor = system.dispatcher
   override implicit val materializer = ActorMaterializer()(system)
+  
+  System.getProperty("bigdl.localMode", "true")
+  System.getProperty("bigdl.coreNumber", "1")
+  Engine.init
+  
+  val models = Models(modelHome)
+  logger.info(s"all models successfully loaded.")
 
   val route = commonRoute(name) ~
     (post & path("textclassification" / "predict") & extract(_.request.entity.contentType) & entity(as[String])) { (contentType, content) =>
+      //TODO models.TCM.predict/infrence
       complete(content)
     } ~
     (post & path("imageclassification" / "predict") & extract(_.request.entity.contentType) & entity(as[String])) { (contentType, content) =>
       complete(content)
     }
-  
-  val models = Models(modelHome)
-  logger.info(s"all models successfully loaded.")
   
   Http().bindAndHandle(route, interface, httpPort)
   logger.info(s"web service ${name} successfully started at ${interface}:${httpPort}.")
